3c71f7ed8ca014c8b4c83bdbb3748ac8989c3f44
Jonas Lundahl <Jonas.Lundahl@outlook.com>
2025-07-26 10:07:13 +0200

    Refactor

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -4,14 +4,20 @@ export function parseSchema(schema = {}, name = "Root") {
   }
 
   if (schema.type === "object") {
-    const properties = Object.entries(schema.properties)
-      .map(([field, def]) => {
-        const isRequired = schema.required?.includes(field);
-        return `${field}: z.unknown()${isRequired ? `,` : `.optional(),`}`;
-      })
-      .join(" ");
-    return `const ${name} = z.object({${properties}});`;
+    return `const ${name} = z.object({${parseObject(schema)}});`;
   }
 
   return `const ${name} = z.unknown();`;
+}
+
+function parseObject(schema = {}, name) {
+  if (!schema) {
+    return "";
+  }
+  return Object.entries(schema.properties)
+    .map(([field, def]) => {
+      const isRequired = schema.required?.includes(field);
+      return `${field}: z.unknown()${isRequired ? `,` : `.optional(),`}`;
+    })
+    .join(" ");
 }
