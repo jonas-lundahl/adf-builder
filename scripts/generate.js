2594dfd2af37e50c17f99ba66cb1687a3d0f8da2
Jonas Lundahl <Jonas.Lundahl@outlook.com>
2025-07-26 13:13:54 +0200

    Rename files

--- scripts/generate.js
+++ scripts/generate.js
@@ -10,12 +10,12 @@ const schema = await fetch(SCHEMA_URL)
 
 await writeFile("./__generated__/schema.json", JSON.stringify(schema, null, 2));
 
-const types = Object.entries(schema.definitions)
+const generate = Object.entries(schema.definitions)
   .map(([name, def]) => parseSchema(def, name))
   .join("\n\n\n\n");
 
-const ts = `import {z} from 'zod';\n\n` + types;
+const ts = `import {z} from 'zod';\n\n` + generate;
 
 const prettyTs = await prettier.format(ts, { parser: "typescript" });
 
-await writeFile("./__generated__/types.ts", prettyTs);
+await writeFile("./__generated__/schema.ts", prettyTs);
