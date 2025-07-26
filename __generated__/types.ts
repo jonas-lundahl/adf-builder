960fb549e13437548b358e458c6cc530ec77085c
Jonas Lundahl <Jonas.Lundahl@outlook.com>
2025-07-26 09:34:15 +0200

    Required and optional fields

--- __generated__/types.ts
+++ __generated__/types.ts
@@ -1,94 +1,94 @@
-type alignment_mark = { type?: unknown; attrs?: unknown };
+type alignment_mark = { type: unknown; attrs: unknown };
 
-type annotation_mark = { type?: unknown; attrs?: unknown };
+type annotation_mark = { type: unknown; attrs: unknown };
 
-type backgroundColor_mark = { type?: unknown; attrs?: unknown };
+type backgroundColor_mark = { type: unknown; attrs: unknown };
 
 type block_content = unknown;
 
-type blockCard_node = { type?: unknown; attrs?: unknown };
+type blockCard_node = { type: unknown; attrs: unknown };
 
-type blockquote_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type blockquote_node = { type: unknown; attrs?: unknown; content: unknown };
 
 type bodiedExtension_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
-  attrs?: unknown;
-  content?: unknown;
+  attrs: unknown;
+  content: unknown;
 };
 
 type bodiedExtension_with_marks_node = unknown;
 
-type border_mark = { type?: unknown; attrs?: unknown };
+type border_mark = { type: unknown; attrs: unknown };
 
-type breakout_mark = { type?: unknown; attrs?: unknown };
+type breakout_mark = { type: unknown; attrs: unknown };
 
-type bulletList_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type bulletList_node = { type: unknown; attrs?: unknown; content: unknown };
 
-type caption_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type caption_node = { type: unknown; attrs?: unknown; content?: unknown };
 
 type code_inline_node = unknown;
 
-type code_mark = { type?: unknown };
+type code_mark = { type: unknown };
 
 type codeBlock_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
   content?: unknown;
 };
 
 type codeBlock_root_only_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
   content?: unknown;
 };
 
-type dataConsumer_mark = { type?: unknown; attrs?: unknown };
+type dataConsumer_mark = { type: unknown; attrs: unknown };
 
-type date_node = { type?: unknown; attrs?: unknown };
+type date_node = { type: unknown; attrs: unknown };
 
-type decisionItem_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type decisionItem_node = { type: unknown; attrs: unknown; content?: unknown };
 
-type decisionList_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type decisionList_node = { type: unknown; attrs: unknown; content: unknown };
 
-type doc_node = { type?: unknown; content?: unknown; version?: unknown };
+type doc_node = { type: unknown; content: unknown; version: unknown };
 
-type em_mark = { type?: unknown };
+type em_mark = { type: unknown };
 
-type embedCard_node = { type?: unknown; attrs?: unknown };
+type embedCard_node = { type: unknown; attrs: unknown };
 
-type emoji_node = { type?: unknown; attrs?: unknown };
+type emoji_node = { type: unknown; attrs: unknown };
 
 type expand_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
-  content?: unknown;
+  content: unknown;
 };
 
 type expand_root_only_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
-  content?: unknown;
+  content: unknown;
 };
 
-type extension_node = { type?: unknown; marks?: unknown; attrs?: unknown };
+type extension_node = { type: unknown; marks?: unknown; attrs: unknown };
 
 type extension_with_marks_node = unknown;
 
 type formatted_text_inline_node = unknown;
 
-type fragment_mark = { type?: unknown; attrs?: unknown };
+type fragment_mark = { type: unknown; attrs: unknown };
 
-type hardBreak_node = { type?: unknown; attrs?: unknown };
+type hardBreak_node = { type: unknown; attrs?: unknown };
 
 type heading_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
-  attrs?: unknown;
+  attrs: unknown;
   content?: unknown;
 };
 
@@ -98,63 +98,59 @@ type heading_with_indentation_node = unknown;
 
 type heading_with_no_marks_node = unknown;
 
-type indentation_mark = { type?: unknown; attrs?: unknown };
+type indentation_mark = { type: unknown; attrs: unknown };
 
 type inline_node = unknown;
 
-type inlineCard_node = { type?: unknown; attrs?: unknown };
+type inlineCard_node = { type: unknown; attrs: unknown };
 
-type inlineExtension_node = {
-  type?: unknown;
-  marks?: unknown;
-  attrs?: unknown;
-};
+type inlineExtension_node = { type: unknown; marks?: unknown; attrs: unknown };
 
 type inlineExtension_with_marks_node = unknown;
 
-type layoutColumn_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type layoutColumn_node = { type: unknown; attrs: unknown; content: unknown };
 
 type layoutSection_full_node = unknown;
 
 type layoutSection_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
-  content?: unknown;
+  content: unknown;
 };
 
-type link_mark = { type?: unknown; attrs?: unknown };
+type link_mark = { type: unknown; attrs: unknown };
 
-type listItem_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type listItem_node = { type: unknown; attrs?: unknown; content: unknown };
 
-type media_node = { type?: unknown; marks?: unknown; attrs?: unknown };
+type media_node = { type: unknown; marks?: unknown; attrs: unknown };
 
-type mediaGroup_node = { type?: unknown; content?: unknown };
+type mediaGroup_node = { type: unknown; content: unknown };
 
-type mediaInline_node = { type?: unknown; marks?: unknown; attrs?: unknown };
+type mediaInline_node = { type: unknown; marks?: unknown; attrs: unknown };
 
 type mediaSingle_caption_node = unknown;
 
 type mediaSingle_full_node = unknown;
 
-type mediaSingle_node = { type?: unknown; marks?: unknown; attrs?: unknown };
+type mediaSingle_node = { type: unknown; marks?: unknown; attrs?: unknown };
 
-type mention_node = { type?: unknown; attrs?: unknown };
+type mention_node = { type: unknown; attrs: unknown };
 
 type nestedExpand_content = unknown;
 
-type nestedExpand_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type nestedExpand_node = { type: unknown; attrs: unknown; content: unknown };
 
 type nestedExpand_with_no_marks_node = unknown;
 
 type non_nestable_block_content = unknown;
 
-type orderedList_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type orderedList_node = { type: unknown; attrs?: unknown; content: unknown };
 
-type panel_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type panel_node = { type: unknown; attrs: unknown; content: unknown };
 
 type paragraph_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
   content?: unknown;
@@ -166,41 +162,41 @@ type paragraph_with_indentation_node = unknown;
 
 type paragraph_with_no_marks_node = unknown;
 
-type placeholder_node = { type?: unknown; attrs?: unknown };
+type placeholder_node = { type: unknown; attrs: unknown };
 
-type rule_node = { type?: unknown; attrs?: unknown };
+type rule_node = { type: unknown; attrs?: unknown };
 
-type status_node = { type?: unknown; attrs?: unknown };
+type status_node = { type: unknown; attrs: unknown };
 
-type strike_mark = { type?: unknown };
+type strike_mark = { type: unknown };
 
-type strong_mark = { type?: unknown };
+type strong_mark = { type: unknown };
 
-type subsup_mark = { type?: unknown; attrs?: unknown };
+type subsup_mark = { type: unknown; attrs: unknown };
 
 type table_cell_content = unknown;
 
-type table_cell_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type table_cell_node = { type: unknown; attrs?: unknown; content: unknown };
 
-type table_header_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type table_header_node = { type: unknown; attrs?: unknown; content: unknown };
 
 type table_node = {
-  type?: unknown;
+  type: unknown;
   marks?: unknown;
   attrs?: unknown;
-  content?: unknown;
+  content: unknown;
 };
 
-type table_row_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type table_row_node = { type: unknown; attrs?: unknown; content: unknown };
 
-type taskItem_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type taskItem_node = { type: unknown; attrs: unknown; content?: unknown };
 
-type taskList_node = { type?: unknown; attrs?: unknown; content?: unknown };
+type taskList_node = { type: unknown; attrs: unknown; content: unknown };
 
-type text_node = { type?: unknown; marks?: unknown; text?: unknown };
+type text_node = { type: unknown; marks?: unknown; text: unknown };
 
 type text_with_no_marks_node = unknown;
 
-type textColor_mark = { type?: unknown; attrs?: unknown };
+type textColor_mark = { type: unknown; attrs: unknown };
 
-type underline_mark = { type?: unknown };
+type underline_mark = { type: unknown };
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,7 +5,10 @@ export function parseSchema(schema = {}, name = "Root") {
 
   if (schema.type === "object") {
     const properties = Object.entries(schema.properties)
-      .map(([field, def]) => `${field}?: unknown;`)
+      .map(([field, def]) => {
+        const colon = schema.required?.includes(field) ? ":" : "?:";
+        return `${field}${colon} unknown;`;
+      })
       .join("\n");
     return `type ${name} = {${properties}};`;
   }
